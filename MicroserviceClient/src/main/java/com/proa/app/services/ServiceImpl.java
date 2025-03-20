9f811ba75c36fd5d603de7e56f4b3d53aad42a69
Mario.nvf <mario.ruiz@gmail.com>
2025-03-19 19:50:53 -0600

    Hola

---
+++ MicroserviceClient/src/main/java/com/proa/app/services/ServiceImpl.java
@@ -0,0 +1,57 @@
+package com.proa.app.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.proa.app.dao.IclientDAO;
+import com.proa.app.entities.Client;
+import com.proa.app.exceptions.ClientNotFoundException;
+
+@Service
+public class ServiceImpl implements IService{
+
+	@Autowired
+	private IclientDAO dao;
+	
+	@Override
+	public boolean insert(Client c) {
+		Client response = null;
+		if (c.getId()==0){
+			response=dao.save(c);
+		}
+		return response !=null;
+	}
+
+	@Override
+	public List<Client> selectAll() {
+		return (List<Client>)dao.findAll();
+
+	}
+
+	@Override
+	public boolean update(Client c) throws ClientNotFoundException{
+		if(dao.existsById(c.getId())){
+			var response=dao.save(c);
+			return response!=null;
+		}
+		var message = "Client " +c.getId()+" not found";
+		throw new ClientNotFoundException(message);
+	}
+
+	@Override
+	public Client findById(long id) throws ClientNotFoundException{
+		return dao.findById(id).orElseThrow(()->new ClientNotFoundException("Client " + id + " not found"));
+	}
+
+	@Override
+	public boolean delete(long id) throws ClientNotFoundException {
+		if (dao.existsById(id)) {
+			dao.deleteById(id);
+			return true;
+		}
+		throw new ClientNotFoundException("Client "+id+"not found!!");
+	}
+
+}
